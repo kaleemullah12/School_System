6397ec48131c10a7608e593b32aa2527ea95f0ea
muneebmazhar <43905103+muneebmazhar@users.noreply.github.com>
2018-11-09 11:22:08 +0500

    kjkjkjk

--- SchoolErp/SchoolErp/Controllers/HomeController.cs
+++ SchoolErp/SchoolErp/Controllers/HomeController.cs
@@ -9,28 +9,50 @@ namespace SchoolErp.Controllers
 {
     public class HomeController : Controller
     {
+        InvictusSchoolEntities db = new InvictusSchoolEntities();
         public ActionResult menu()
         {
             return View();
         }
-        public ActionResult Index()
+        public ActionResult Login()
         {
             return View();
         }
+        
         [HttpPost]
         public ActionResult Login(Admin data)
         {
-            InvictusSchoolEntities db = new InvictusSchoolEntities();
+
             var rec = db.Admins.Where(x => x.User_Name == data.User_Name && x.Password == data.Password).SingleOrDefault();
             Session["admin"] = rec;
-            return RedirectToAction("Dashboard");
+            if (Session["Admin"] != null)
+            {
+                return RedirectToAction("Dashboard");
+            }
+            else
+            {
+                ViewBag.msg = "Your User Name Or Password is Incorrect!!";
+                return View();
+            }
         }
         public ActionResult Dashboard()
         {
-            if (Session["admin"] != null) { 
-            return View();
+            if (Session["admin"] != null)
+            {
+                var list = db.Student_Records.ToList();
+                ViewBag.total = list.Count();
+                var li = db.Classes.ToList();
+                ViewBag.cl = li.Count();
+                var sc = db.Staffs.ToList();
+                ViewBag.sec = sc.Count();
+                return View();
             }
-            return Content("This is the Admin Panel");
+            return RedirectToAction("Login");
+        }
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Login");
         }
     }
 }
