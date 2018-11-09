9a996ccc70595ca917c4f6a271e6bd2c02646cfe
kaleemullah12 <37169000+kaleemullah12@users.noreply.github.com>
2018-11-08 22:25:58 -0800

    sdas

---
+++ SchoolErp/SchoolErp/Controllers/Cl_SecController.cs
@@ -0,0 +1,28 @@
+﻿using SchoolErp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolErp.Controllers
+{
+    public class Cl_SecController : Controller
+    {
+        InvictusSchoolEntities db = new InvictusSchoolEntities();
+        // GET: Cl_Sec
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            var sec_list = db.Sections.ToList();
+            ViewBag.sc = sec_list;
+            var cal_list = db.Classes.ToList();
+            ViewBag.cl = cal_list;
+            return View();
+        }
+    }
+}
---
+++ SchoolErp/SchoolErp/jquery/Cl_Sec.js
@@ -0,0 +1,72 @@
+﻿var hashtable = [];
+$(document).ready(function () {
+
+});
+
+
+function Add() {
+    var section = $('#section option:selected').val();
+    var sectionName = $('#section option:selected').text();
+    if (!section) {
+        ShowError('Section is required!');
+        return;
+    }
+    debugger;
+   
+    var isFound = 0;
+    for (i = 0; i < hashtable.length; i++) {
+        if (hashtable[i].section == section) {
+            isFound = 1;
+            hashtable[i].section = section;
+           
+            ShowSuccess('Section is updated!');
+        }
+    }
+    if (isFound == 0) {
+        hashtable.push({ section: section, Name: sectionName });
+        ShowSuccess('Section added!');
+    }
+
+    FillContainerFromArray();
+}
+
+function FillContainerFromArray() {
+    debugger;
+    var html = '';
+    $("#container_List").empty();
+    if (hashtable.length == 0) {
+        html += "<tr><td>No item added</td></tr>";
+    }
+    for (i = 0; i < hashtable.length; i++) {
+        html += '<tr>' +
+                      '<td>' + hashtable[i].Name + '</td>' +
+                      '<td class="text-right">' +
+                           '<a href="javascript:;"><i class="glyphicon glyphicon-pencil" title="Edit" onclick="EditActionItem(' + hashtable[i].section + ')"></i></a>' +
+                           '&nbsp;&nbsp;<a href="javascript:;" onclick="DeleteActionItem(' + hashtable[i].section + ')"><i class="glyphicon glyphicon-trash" title="Delete"></i></a>' +
+                      '</td>' +
+                  '</tr>';
+    }
+    $("#container_List").append(html);
+}
+function EditActionItem(section) {
+    debugger;
+    $('#section').val(section);
+    //$('#section').trigger('change');
+    
+}
+
+
+function DeleteActionItem(itemid) {
+    for (i = 0; i < hashtable.length; i++) {
+        debugger;
+        if (hashtable[i].section == itemid) {
+            ShowSuccess('item deleted!');
+            hashtable.splice(i, 1);
+            break;
+        }
+    }
+
+    FillContainerFromArray();
+  
+}
+
